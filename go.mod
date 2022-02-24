b33185641a953101973bc6071e03b8c5ee62d901
apariani3 <apariani3@gatech.edu>
2022-02-24 18:03:48 -0500

    add repo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module codenamesgreen
+
+go 1.17
---
+++ src/index.js
@@ -0,0 +1,33 @@
+import './main.css';
+import { Elm } from './Main.elm';
+
+// Metadata about the user is persisted in local storage.
+// If there is no user in local storage, we generate one
+// with a random player ID and random guest name.
+var encodedUser = localStorage.getItem('user');
+
+var parsedUser = encodedUser ? JSON.parse(encodedUser) : null;
+
+if (parsedUser == null || !parsedUser.player_id || !parsedUser.name) {
+    // generate a new user with a random identifier and save it.
+
+    var entropy = new Uint32Array(4); // 128 bits
+    window.crypto.getRandomValues(entropy);
+    var playerID = entropy.join("-");
+
+    var guestNumber = Math.floor(Math.random() * 4095);
+
+    encodedUser = JSON.stringify({
+      player_id: playerID,
+      name: 'Guest '+ guestNumber.toString(16).toUpperCase(),
+    });
+    localStorage.setItem('user', encodedUser);
+}
+
+var app = Elm.Main.init({
+  node: document.getElementById('root'),
+  flags: encodedUser,
+});
+app.ports.storeCache.subscribe(function(u) {
+  localStorage.setItem('user', JSON.stringify(u));
+});
